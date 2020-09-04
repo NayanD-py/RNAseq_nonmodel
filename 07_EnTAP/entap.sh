53cd54fe22cb900a181b3f797b5bb14f020c1f19
Noah Reid <noah.reid@uconn.edu>
2020-09-04 10:11:07 -0400

    entap

--- 07_EnTAP/entap.sh
+++ 07_EnTAP/entap.sh
@@ -23,10 +23,11 @@ date
 
 module load seqtk/1.2
 
-grep -oP "(?<=>).*" $CENTROIDS >names.txt
 
 CENTROIDS=../05_Clustering/centroids.fasta
 PEPTIDES=../04_Coding_Regions/trinity_combine.fasta.transdecoder.pep
+
+grep -oP "(?<=>).*" $CENTROIDS >names.txt
 
 seqtk subseq $PEPTIDES names.txt >centroids.pep
  

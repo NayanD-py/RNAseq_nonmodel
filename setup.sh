852fd2c6b5d3262fc9da636a78f43fc8b1962a7f
Noah Reid <noah.reid@uconn.edu>
2020-09-04 11:31:00 -0400

    setup

--- setup.sh
+++ setup.sh
@@ -10,5 +10,21 @@ jid2=$(sbatch --parsable --dependency=afterok:$jid1 qc_raw.sh)
 jid3=$(sbatch --parsable --dependency=afterok:$jid2 trimmomatic.sh)
 jid4=$(sbatch --parsable --dependency=afterok:$jid3 qc_trimmed.sh)
 
+cd ../03_Assembly/
+jid5=$(sbatch --parsable --dependency=afterok:$jid4 trinity.sh)
 
+cd ../04_Coding_Regions/
+jid6=$(sbatch --parsable --dependency=afterok:$jid5 transdecoder.sh)
 
+cd ../05_Clustering/
+jid7=$(sbatch --parsable --dependency=afterok:$jid6 vsearch.sh)
+
+cd ../06_RNAQuast/
+jid8=$(sbatch --parsable --dependency=afterok:$jid7 rnaQuast.sh)
+
+cd ../07_EnTAP/
+jid9=$(sbatch --parsable --dependency=afterok:$jid7 entap.sh)
+
+cd ../08_Counts/
+jid10=$(sbatch --parsable --dependency=afterok:$jid7 kallisto_index.sh)
+jid11=$(sbatch --parsable --dependency=afterok:$jid10 kallisto_counts.sh)
